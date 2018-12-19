38f95f21468cde79a2680290e42a934228f19098
UCY\mtziak01 <mtziak01@ucy.ac.cy>
2018-12-19 12:33:25 +0200

    Change db user pass

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'portofoliodb',
         'USER': 'postgres',
-        'PASSWORD': 'maria1234',
+        'PASSWORD': 'djangoapp',
         'HOST': '127.0.0.1',
         'PORT': '5432',
 
